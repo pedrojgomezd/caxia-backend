0b7f3b5b8cbd18eb71daa2967ca4d135dd386aa9
Pedro J Gomez <pedrojgomez.dev@gmail.com>
2021-02-07 01:51:19 -0300

    Routes app mobil login-logout

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
 use App\Http\Controllers\CustomerController;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,3 +26,34 @@ Route::post('customers/register', [CustomerController::class, 'store']);
 Route::middleware('auth:sanctum')->get('customers/', [CustomerController::class, 'index']);
 Route::middleware('auth:sanctum')->get('customers/{customer}', [CustomerController::class, 'show']);
 Route::middleware('auth:sanctum')->put('customers/{customer}', [CustomerController::class, 'update']);
+
+Route::post('/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required'
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (!$user || !Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    $token = $user->createToken($request->device_name)->plainTextToken;
+
+    $response = [
+        'user' => $user,
+        'token' => $token,
+    ];
+
+    return response($response, 201);
+});
+
+Route::middleware('auth:sanctum')->post('/logout', function (Request $request) {
+    $request->user()->tokens()->delete();
+
+    return response('Loggedout', 200);
+});
